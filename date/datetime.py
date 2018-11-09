b7a178f10613252717304162ce2ca30a775a0357
mengll <1633094010@qq.com>
2018-11-09 17:39:32 +0800

    时间转化

--- date/datetime.py
+++ date/datetime.py
@@ -4,3 +4,23 @@ def get_timestamp_day(day,time_stmp =time.time()):
     day_temp = datetime.datetime.strptime(day_temp,"%Y-%m-%d")
     back = day_temp + datetime.timedelta(days=day)
     return int(back.timestamp())
+
+
+# python 时间格式转化 设置时区
+LOCAL_TIME_DELTA = datetime.timedelta(hours=8) 
+def get_time(self, item):
+        if item['create_ts']:
+            create_ts = timestamp2string(item['create_ts'])
+        else:
+            start_time = item['created_at']
+            if str(start_time).__contains__('.') and not str(start_time).__contains__('T'):
+                create_ts = convert_datetime_to_timestamp(datetime.datetime.strptime(str(start_time),
+                                                        "%Y-%m-%d %H:%M:%S.%f") + self.LOCAL_TIME_DELTA)
+            elif str(start_time).__contains__('T'):
+                create_ts = convert_datetime_to_timestamp(datetime.datetime.strptime(str(start_time),
+                                                        "%Y-%m-%dT%H:%M:%S.%f") + self.LOCAL_TIME_DELTA)
+            else:
+                create_ts = convert_datetime_to_timestamp(
+                    datetime.datetime.strptime(str(start_time), "%Y-%m-%d %H:%M:%S") + self.LOCAL_TIME_DELTA)
+
+        return create_ts
